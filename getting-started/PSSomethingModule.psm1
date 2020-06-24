dd657d2e84dc9e62f626fc58adf6075f14c946b7
Jeff Brown Tech <jeff@jeffbrown.tech>
2020-06-24 06:29:21 -0500

    basic module written

---
+++ getting-started/PSSomethingModule.psm1
@@ -0,0 +1,14 @@
+function Get-Something {
+    Write-Output "I got something!"
+}
+
+function Set-Something {
+    Write-Output "I set something!"
+}
+
+function Test-Something {
+    Write-Output "I tested something!"
+}
+
+
+
